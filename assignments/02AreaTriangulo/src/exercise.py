aae38ed32baf63119dab91673c2751b8271c317d
alanburgos1 <A01742473@tec.mx>
2021-08-16 23:29:12 +0000

    tarea 1 gitpod

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    n1 = float(input('Dame la base: '))
+    n2 = float(input('Dame la altura: ')) 
+
+    area = n1 * n2 / 2
+
+    print('El área es: '+str(area))
 
 if __name__ == '__main__':
     main()
--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    n1 = float(input('Calificación de la materia: '))
+    n2 = float(input('Calificación de la materia: '))
+    n3 = float(input('Calificación de la materia: '))
+    n4 = float(input('Calificación de la materia: '))
+
+    suma = n1 + n2 + n3 + n4
+    promedio = suma / 4
+
+    print('El promedio es: '+str(promedio))
+
 
 
 if __name__ == '__main__':
